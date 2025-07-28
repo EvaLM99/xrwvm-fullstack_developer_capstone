b293ceddb3b93a2e4affded5380bb5113197d22c
EvaLM99 <lemao.eva@gmail.com>
2025-07-28 13:02:17 -0400

    Added changes of Module 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -116,13 +116,18 @@ def get_dealer_reviews(request, dealer_id):
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 # ...
-def get_dealer_details(request,dealer_id):
-    if (dealer_id):
-        endpoint = "/fetchDealers/"+str(dealer_id)
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
         dealerships = get_request(endpoint)
-        return JsonResponse({"status":200,"dealers":dealerships})
-    else : 
-        return JsonResponse({"status":400,"message" : "Bad Request"})
+        if dealerships and isinstance(dealerships, list) and len(dealerships) > 0:
+            dealer = dealerships[0]
+            return JsonResponse({"status": 200, "dealer": dealer})
+        else:
+            return JsonResponse({"status": 404, "message": "Dealer not found"})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
